416b975e9dbf3d84bdc47bb9f22b1e2aa0f01b3d
medsysbot <medisys.bot@gmail.com>
2025-04-03 14:44:29 -0300

    Add files via upload

---
+++ static/botones.js
@@ -0,0 +1,11 @@
+
+function guardarPDF() {
+    alert("Guardado correctamente.");
+}
+function eliminarPDF() {
+    const confirmacion = confirm("¿Seguro que deseas eliminar este archivo?");
+    if (confirmacion) {
+        alert("Eliminado.");
+    }
+}
+    
