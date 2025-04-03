c10c72b3c555689fb4b49640710d83e375041a9c
medsysbot <medisys.bot@gmail.com>
2025-04-03 17:26:51 -0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,13 @@
+function guardarFormulario() {
+    alert("Formulario guardado (simulado).");
+}
 
+function imprimirFormulario() {
+    window.print();
+}
+
+function eliminarFormulario() {
+    if (confirm("¿Estás seguro de eliminar este formulario?")) {
+        alert("Formulario eliminado (simulado).");
+    }
+}
