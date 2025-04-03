9c642bad190cd8d2b024c1e4c56c6d33b405ca4b
medsysbot <medisys.bot@gmail.com>
2025-04-03 20:58:08 -0300

    Delete static/Js/js directory

--- static/Js/js/acciones.js
+++
@@ -1,9 +0,0 @@
-
-function guardarFormulario() {
-  alert("Formulario guardado.");  // Esto después se conecta al backend real
-}
-
-function eliminarFormulario() {
-  const form = document.querySelector("form");
-  if (form) form.reset();
-}
