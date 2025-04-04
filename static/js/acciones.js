e68cfde0b7c7593a9ce402d64a9dffbe28d31651
medsysbot <medisys.bot@gmail.com>
2025-04-03 21:17:00 -0300

    Update acciones.js

--- static/js/acciones.js
+++ static/js/acciones.js
@@ -1,1 +1,26 @@
+function imprimirPDF() {
+    const iframe = document.querySelector('iframe');
+    if (iframe && iframe.contentWindow) {
+        iframe.contentWindow.focus();
+        iframe.contentWindow.print();
+    } else {
+        alert("No se encontró el documento PDF para imprimir.");
+    }
+}
 
+function eliminarPDF() {
+    const visor = document.getElementById('pdf-visor');
+    if (visor) {
+        visor.setAttribute('src', '');
+        alert("PDF eliminado del visor.");
+    }
+}
+
+function enviarFormulario(formId) {
+    const form = document.getElementById(formId);
+    if (form) {
+        form.submit();
+    } else {
+        alert("Formulario no encontrado.");
+    }
+}
