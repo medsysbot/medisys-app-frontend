e9182a67b50bc7efc4f3601a1c99550b4efa7f17
medsysbot <medisys.bot@gmail.com>
2025-04-04 13:22:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,33 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-# Monta los archivos estáticos (CSS, imágenes, JS)
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
-# Define carpeta de templates
 templates = Jinja2Templates(directory="templates")
 
-# Ruta de inicio
 @app.get("/", response_class=HTMLResponse)
-async def inicio(request: Request):
+async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-# Vistas individuales
-@app.get("/historia_completa", response_class=HTMLResponse)
-async def historia_completa(request: Request):
+@app.get("/historia_clinica", response_class=HTMLResponse)
+async def historia_clinica(request: Request):
     return templates.TemplateResponse("historia_completa.html", {"request": request})
-
-@app.get("/historia_resumida", response_class=HTMLResponse)
-async def historia_resumida(request: Request):
-    return templates.TemplateResponse("historia_resumida.html", {"request": request})
 
 @app.get("/evolucion", response_class=HTMLResponse)
 async def evolucion(request: Request):
     return templates.TemplateResponse("evolucion.html", {"request": request})
 
-@app.get("/receta", response_class=HTMLResponse)
-async def receta(request: Request):
+@app.get("/historia_resumida", response_class=HTMLResponse)
+async def historia_resumida(request: Request):
+    return templates.TemplateResponse("historia_resumida.html", {"request": request})
+
+@app.get("/recetas", response_class=HTMLResponse)
+async def recetas(request: Request):
     return templates.TemplateResponse("receta.html", {"request": request})
 
 @app.get("/indicaciones", response_class=HTMLResponse)
 async def indicaciones(request: Request):
     return templates.TemplateResponse("indicaciones.html", {"request": request})
-
-@app.get("/turnos", response_class=HTMLResponse)
-async def turnos(request: Request):
-    return templates.TemplateResponse("turnos.html", {"request": request})
-
-@app.get("/registro", response_class=HTMLResponse)
-async def registro_pacientes(request: Request):
-    return templates.TemplateResponse("registro.html", {"request": request})
