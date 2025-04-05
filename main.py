a59ff5833a97dcfe5d3a66ad5517f5cf67f20ad4
medsysbot <medisys.bot@gmail.com>
2025-04-04 21:42:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,16 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
+from flask import Flask, render_template
 
-app = FastAPI()
+app = Flask(__name__, template_folder="templates", static_folder="static")
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+@app.route("/")
+def index():
+    return render_template("index.html")
 
+@app.route("/registro")
+def registro():
+    return render_template("registro.html")
 
-@app.get("/", response_class=HTMLResponse)
-async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+# 🔹 Agregá más rutas cuando lo necesites (historia, receta, etc.)
 
-
-@app.get("/registro", response_class=HTMLResponse)
-async def registro_pacientes(request: Request):
-    return templates.TemplateResponse("registro.html", {"request": request})
-
-
-@app.get("/historia", response_class=HTMLResponse)
-async def historia_clinica(request: Request):
-    return templates.TemplateResponse("historia.html", {"request": request})
-
-
-@app.get("/recetas", response_class=HTMLResponse)
-async def recetas(request: Request):
-    return templates.TemplateResponse("recetas.html", {"request": request})
-
-
-@app.get("/indicaciones", response_class=HTMLResponse)
-async def indicaciones_medicas(request: Request):
-    return templates.TemplateResponse("indicaciones.html", {"request": request})
-
-
-@app.get("/turnos", response_class=HTMLResponse)
-async def turnos(request: Request):
-    return templates.TemplateResponse("turnos.html", {"request": request})
+if __name__ == "__main__":
+    app.run(debug=True)
